b134e46d962618d92343eafcbe38b944db15d19d
tigeproverbes194-sudo <tigeproverbes194@gmail.com>
2026-05-20 11:54:28 +0100

    Correction classe Commune

--- commune.java
+++ commune.java
@@ -2,20 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Commune {
-    
+
     private String nom;
-    private List<Quartier> quartiers;
+    private List<String> quartiers;
+    private List<String> habitants;
 
     public Commune(String nom) {
         this.nom = nom;
         this.quartiers = new ArrayList<>();
+        this.habitants = new ArrayList<>();
     }
 
-    public void ajouterQuartier(Quartier q) {
-        quartiers.add(q);
+    public void ajouterQuartier(String quartier) {
+        quartiers.add(quartier);
     }
 
-    public List<Quartier> getQuartiers() {
+    public void ajouterHabitant(String habitant) {
+        habitants.add(habitant);
+    }
+
+    public List<String> getHabitants() {
+        return habitants;
+    }
+
+    public List<String> getQuartiers() {
         return quartiers;
     }
 
@@ -25,6 +35,10 @@ public class Commune {
 
     public static void main(String[] args) {
         Commune commune = new Commune("Commune Centrale");
+        commune.ajouterQuartier("Quartier Nord");
+        commune.ajouterHabitant("Jean Dupont");
         System.out.println("Commune : " + commune.getNom());
+        System.out.println("Quartiers : " + commune.getQuartiers());
+        System.out.println("Habitants : " + commune.getHabitants());
     }
 }
